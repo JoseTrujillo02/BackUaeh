8d0fca2d43669c201030373020daed8ed0cfb0bf
Jose De Jesus Buendia Trujillo <22300029@uttt.edu.mx>
2026-04-21 12:52:26 -0600

    mas cors

--- main.py
+++ main.py
@@ -101,140 +101,140 @@ def obtener_aulas_por_materia(nombre):
 # =========================
 # ENDPOINT
 # =========================
-@app.post("/generar-horarios")
-def generar_horarios(materias: List[Materia]):
+#@app.post("/generar-horarios")
+#def generar_horarios(materias: List[Materia]):#
 
-    materias_limpias = []
+#    materias_limpias = []#
 
-    for m in materias:
+#    for m in materias:#
 
-        data = m.dict()
+#        data = m.dict()#
 
-        try:
-            semestre = int(m.Semestre)
-        except:
-            semestre = 0
+#        try:
+#            semestre = int(m.Semestre)
+#        except:
+#            semestre = 0#
 
-        grupo = str(m.Grupo)
-        profesor = data.get("NombreProfesor", "Sin profesor")
-        turno = data.get("Turno", "M")
-        nombre = data.get("NombreAsignatura", "")
+#        grupo = str(m.Grupo)
+#        profesor = data.get("NombreProfesor", "Sin profesor")
+#        turno = data.get("Turno", "M")
+#        nombre = data.get("NombreAsignatura", "")#
 
-        horario_limpio = []
+#        horario_limpio = []#
 
-        for h in m.Horario:
-            if isinstance(h, dict):
-                if "dia" in h and "inicio" in h and "fin" in h:
-                    horario_limpio.append({
-                        "dia": h["dia"],
-                        "inicio": int(h["inicio"]),
-                        "fin": int(h["fin"])
-                    })
+#        for h in m.Horario:
+#            if isinstance(h, dict):
+#                if "dia" in h and "inicio" in h and "fin" in h:
+#                    horario_limpio.append({
+#                        "dia": h["dia"],
+#                        "inicio": int(h["inicio"]),
+#                        "fin": int(h["fin"])
+#                    })#
 
-        materias_limpias.append({
-            **data,
-            "Semestre": semestre,
-            "Grupo": grupo,
-            "NombreProfesor": profesor,
-            "Turno": turno,
-            "NombreAsignatura": nombre,
-            "Horario": horario_limpio
-        })
+#        materias_limpias.append({
+#            **data,
+#            "Semestre": semestre,
+#            "Grupo": grupo,
+#            "NombreProfesor": profesor,
+#            "Turno": turno,
+#            "NombreAsignatura": nombre,
+#            "Horario": horario_limpio
+#        })#
 
-    materias = materias_limpias
+#    materias = materias_limpias#
 
-    # =========================
-    # ORTOOLS
-    # =========================
-    model = cp_model.CpModel()
+#    # =========================
+#    # ORTOOLS
+#    # =========================
+#    model = cp_model.CpModel()#
 
-    solver = cp_model.CpSolver()
-    solver.parameters.max_time_in_seconds = 5
+#    solver = cp_model.CpSolver()
+#    solver.parameters.max_time_in_seconds = 5#
 
-    # =========================
-    # RESPUESTA FINAL
-    # =========================
-    resultado = []
+#    # =========================
+#    # RESPUESTA FINAL
+#    # =========================
+#    resultado = []#
 
-    dias_reparto = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
+#    dias_reparto = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"]#
 
-    ocupacion_global = {}
-    control_grupos = {}
-    
-    for materia in materias:
-    
-        clave = f"{materia['Semestre']}-{materia['Grupo']}"
-    
-        if clave not in ocupacion_global:
-            ocupacion_global[clave] = {
-                dia: {} for dia in dias_reparto
-            }
-    
-        if clave not in control_grupos:
-            control_grupos[clave] = {
-                "dia_index": 0,
-                "hora": 7 if materia.get("Turno", "M") != "V" else 13
-            }
-    
-        salida = {
-            **materia,
-            "AulaAsignada": []
-        }
-    
-        aulas_disponibles = obtener_aulas_por_materia(
-            materia.get("NombreAsignatura", "")
-        )
-    
-        aula_index = 0
-    
-        turno = materia.get("Turno", "M")
-    
-        limite = 17 if turno == "V" else 12
-    
-        for h in materia["Horario"]:
-        
-            total_horas = h["fin"] - h["inicio"]
-    
-            while total_horas > 0:
-            
-                dia_actual = dias_reparto[
-                    control_grupos[clave]["dia_index"] % len(dias_reparto)
-                ]
-    
-                hora_inicio = control_grupos[clave]["hora"]
-    
-                aula_actual = aulas_disponibles[aula_index % len(aulas_disponibles)]
-    
-                if aula_actual not in ocupacion_global[clave][dia_actual]:
-                    ocupacion_global[clave][dia_actual][aula_actual] = set()
-    
-                conflicto = hora_inicio in ocupacion_global[clave][dia_actual][aula_actual]
-    
-                if conflicto:
-                    aula_index += 1
-                    continue
-                
-                ocupacion_global[clave][dia_actual][aula_actual].add(hora_inicio)
-    
-                salida["AulaAsignada"].append({
-                    "dia": dia_actual,
-                    "inicio": hora_inicio,
-                    "fin": hora_inicio + 1,
-                    "aula": aula_actual,
-                    "profesor": materia.get("NombreProfesor", "")
-                })
-    
-                control_grupos[clave]["hora"] += 1
-    
-                if control_grupos[clave]["hora"] > limite:
-                    control_grupos[clave]["hora"] = 13 if turno == "V" else 7
-                    control_grupos[clave]["dia_index"] += 1
-    
-                total_horas -= 1
-    
-        resultado.append(salida)
+#    ocupacion_global = {}
+#    control_grupos = {}
+#    
+#    for materia in materias:
+#    
+#        clave = f"{materia['Semestre']}-{materia['Grupo']}"
+#    
+#        if clave not in ocupacion_global:
+#            ocupacion_global[clave] = {
+#                dia: {} for dia in dias_reparto
+#            }
+#    
+#        if clave not in control_grupos:
+#            control_grupos[clave] = {
+#                "dia_index": 0,
+#                "hora": 7 if materia.get("Turno", "M") != "V" else 13
+#            }
+#    
+#        salida = {
+#            **materia,
+#            "AulaAsignada": []
+#        }
+#    
+#        aulas_disponibles = obtener_aulas_por_materia(
+#            materia.get("NombreAsignatura", "")
+#        )
+#    
+#        aula_index = 0
+#    
+#        turno = materia.get("Turno", "M")
+#    
+#        limite = 17 if turno == "V" else 12
+#    
+#        for h in materia["Horario"]:
+#        
+#            total_horas = h["fin"] - h["inicio"]
+#    
+#            while total_horas > 0:
+#            
+#                dia_actual = dias_reparto[
+#                    control_grupos[clave]["dia_index"] % len(dias_reparto)
+#                ]
+#    
+#                hora_inicio = control_grupos[clave]["hora"]
+#    
+#                aula_actual = aulas_disponibles[aula_index % len(aulas_disponibles)]
+#    
+#                if aula_actual not in ocupacion_global[clave][dia_actual]:
+#                    ocupacion_global[clave][dia_actual][aula_actual] = set()
+#    
+#                conflicto = hora_inicio in ocupacion_global[clave][dia_actual][aula_actual]
+#    
+#                if conflicto:
+#                    aula_index += 1
+#                    continue
+#                
+#                ocupacion_global[clave][dia_actual][aula_actual].add(hora_inicio)
+#    
+#                salida["AulaAsignada"].append({
+#                    "dia": dia_actual,
+#                    "inicio": hora_inicio,
+#                    "fin": hora_inicio + 1,
+#                    "aula": aula_actual,
+#                    "profesor": materia.get("NombreProfesor", "")
+#                })
+#    
+#                control_grupos[clave]["hora"] += 1
+#    
+#                if control_grupos[clave]["hora"] > limite:
+#                    control_grupos[clave]["hora"] = 13 if turno == "V" else 7
+#                    control_grupos[clave]["dia_index"] += 1
+#    
+#                total_horas -= 1
+#    
+#        resultado.append(salida)#
 
-    return resultado
+#    return resultado
 
 #
 #ARCHIVOS EXCEL
